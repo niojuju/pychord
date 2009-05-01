2cb74a398cdb0c9afbe3d9f6ff8efe8d376d005c
satpreetsingh <satpreetsingh@d6cf1f7e-1fa8-11de-82b5-194b17410922>
2009-05-01 00:26:19 +0000

    general commit, no large features added

--- trunk/chordTest.py
+++ trunk/chordTest.py
@@ -3,7 +3,8 @@ Constants:
 - SIZE_OF_NAMESPACE = 2^16 ??
 - MAX_NODES = 2^10 = 1024 currently
 - MAX_CHURN_PERCENT = 2 
-- VISUALIZE = true
+- VISUALIZE
+- LOG_TO_FILE
 - MAX_MESSAGES (generated in any tick) = 1% x MAX_NODES
 - CHURN_PDF = {Uniform, Poisson, Weibull}
 
@@ -29,17 +30,27 @@ Simulation with Churn:
 # IMPORTS
 from math import log
 from time import sleep
+from random import randint 
+
 import pychord
 import chordViz
 import chordLogger
 
 
-# CONSTANTS
-VISUALIZE = True
-SIZE_OF_NAMESPACE = 2 ** 16
+# Simulation CONSTANTS
+TICKS_DURATION = 2**5
+
+# Chord/Network  CONSTANTS
+SIZE_OF_NAMESPACE = 2**16
 MAX_NODES = 64      # make 1024 later
+MAX_MESSAGES = MAX_NODES/10   # (generated in any tick)
+
+# Logging  CONSTANTS
+VISUALIZE = True
+LOG_TO_FILE = True
+
+# Churn CONSTANTS
 MAX_CHURN_PERCENT = 2 
-MAX_MESSAGES = (1 / 100) * MAX_NODES    # (generated in any tick)
 CHURN_PDF = ['Uniform', 'Poisson', 'Weibull']
 
 
@@ -47,47 +58,44 @@ class ChordTest:
    """ simple test. full network, all 16 nodes are there, all routing tables are set """   
 
    def __init__(self):
-      """
       self.nodes = []
       self.t = 0
 
-      #init nodes
-      for i in range(SIZE):
-         self.nodes.append(Node(i))
-      
-      #init routing tables
-      
-      for i in range(SIZE):
-         self.nodes[i].fingers = []
-         #print log(SIZE,2), "links"
-         for j in range(log(SIZE,2)):
-            #print "link to :", (i+(2**j))%SIZE, i, j
-            self.nodes[i].fingers.append(self.nodes[(i+(2**j))%SIZE])
 
-
-
-      #test messages
-      from random import randint 
-      #send_message(self.nodes[1], Message(randint(1,SIZE-1),randint(1,SIZE-1),i)) #random message
-      send_message(self.nodes[1], Message(1,52,1)) #random message
-      send_message(self.nodes[1], Message(45,12,5)) #random message
-      """      
-
-   def tick(self):
-      self.t += 1
-      # print "tick", self.t
-      for n in self.nodes:
-         n.tick(self.t)
-
-
+   def add_new_messages(nw):
+        #test messages
+        from random import randint 
+        #send_message(self.nodes[1], Message(randint(1,SIZE-1),randint(1,SIZE-1),i)) #random message
+        send_message(self.nodes[1], Message(1,52,1)) #random message
+        send_message(self.nodes[1], Message(45,12,5)) #random message
+        return
 
 if __name__ == "__main__":
-   tester = ChordTest()
-   visualizer = ChordWindow()
-   logger = chordLogger()
+   """ Make class instances """ 
+   # tester = ChordTest()
+   # logger = chordLogger()
+   nw = pychord.Network(MAX_NODES) # should be SIZE_OF_NAMESPACE
+
+   """ Bootstrap/Initialize network """ 
+   # add a fixed number of nodes per tick
+   # keep track of nodes that have been added
+   # nodes should have random ID (assume no collisions? or check)
+   # stop at MAX_NODES * JOIN_DURATION
    
+   """ Normal Simulation (No churn) """
    for i in range(10):
        print "Tick", i
-       tester.tick()
-       sleep(1)
+       # add new messages
+       # tick network
+       nw.tick()
+       sleep(0.1)
        
+   """ Simulation with random churn """
+       # add new messages
+       # add churn (joins, leaves)
+       # tick network
+   
+   """ Simulation with adversarial churn """
+   
+   """ Un-bootstrap network (just for fun) """
+       # remove a random node from the list
